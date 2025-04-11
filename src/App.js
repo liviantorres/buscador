bd026ca99a4d39f04a15c0a027c6f7e3dc6cff94
Livian Torres <liviantorres2@gmail.com>
2025-04-11 12:54:07 -0300

    Inicializando o Buscador

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
+import axios from "axios";
+import "./styles/App.scss";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [books, setBooks] = useState([]);
+
+  const SearchBook = async () => {
+    try {
+      const response = await axios.get(
+        `https://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      const results = response.data.hits ? response.data.hits : [];
+      console.log(response.data.hits);
+      setBooks(results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>DevReads</h1>
+   
+      <div className="inputbutton">
+  <input
+    placeholder="O que você quer ler hoje?"
+    value={query}
+    onChange={(e) => setQuery(e.target.value)}
+    type="text"
+  />
+  <button onClick={SearchBook}>
+    <IoIosSearch size={24} />
+  </button>
+</div>
+
+    
+      {books.length > 0 && (
+        <div className="card-container">
+          {books.map((item) => (
+            <div className="card" key={item.objectID}>
+              <h2>{item.title || "Sem título 😅"}</h2>
+              <p>
+                <strong>Autor:</strong> {item.author || "Desconhecido"}
+              </p>
+              {item.url && (
+                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                  Ler artigo →
+                </a>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
